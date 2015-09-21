9d7e11e5782d988cab6a260f453db02542d12ae5
jenkins <jenkins@teamw.in>
2015-09-21 16:45:44 -0400

    oneplus2: TWRP fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,6 +196,10 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # TWRP
 TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 BOARD_HAS_NO_REAL_SDCARD := true
 TW_NO_SCREEN_TIMEOUT := true
 TW_BOARD_CUSTOM_GRAPHICS := ../../../device/oneplus/oneplus2/graphics.c
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+
