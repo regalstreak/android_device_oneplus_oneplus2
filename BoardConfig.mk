3aef0a0ffb2dd8a1e7398a102597a8e6529463cc
thecrazyskull <anaskarbila@aospa.co>
2016-03-31 03:35:27 -0400

    oneplus2: enable dexpreopt for userdebug
    Change-Id: Ib8fe231cabfb785493ae9d68dd362ce410702179
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,7 +167,7 @@ USE_SENSOR_MULTI_HAL := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user userdebug),true)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
