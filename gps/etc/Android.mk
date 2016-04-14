d9eeacde94719f6d67f8bfbb07eaea28c9874014
Neil <regalstreak@gmail.com>
2016-04-14 16:38:39 +0000

    gps: Again, caf had us

--- gps/etc/Android.mk
+++ gps/etc/Android.mk
@@ -9,14 +9,6 @@ LOCAL_SRC_FILES := flp.conf
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := gps.conf
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/
-LOCAL_SRC_FILES := gps.conf
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := izat.conf
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
