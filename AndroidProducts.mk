baa17ed3afff6f7050b155fe3be4ab40fa7dbd34
Neil Agarwal <regalstreak@gmail.com>
2016-04-25 14:04:29 +0000

    oneplus2: Halogenify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,11 @@
-# Copyright (C) 2015 The CyanogenMod Project
+#
+# Copyright (C) 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/oneplus/oneplus2/full_oneplus2.mk)
+PRODUCT_MAKEFILES := \
+     $(LOCAL_DIR)/aosp_oneplus2.mk
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
--- aosp_oneplus2.mk
+++ aosp_oneplus2.mk
@@ -1,5 +1,4 @@
-#
-# Copyright 2015 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,19 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/oneplus/oneplus2/full_oneplus2.mk)
 
-# Inherit from oneplus2 device
-$(call inherit-product, device/oneplus/oneplus2/device.mk)
-
-PRODUCT_NAME := aosp_oneplus2
-PRODUCT_DEVICE := oneplus2
-PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_MODEL := oneplus2
-PRODUCT_BRAND := OnePlus
-
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
