57ea69bbb341f64c2b1e9538f424bde68d547435
Harsh Shandilya <msfjarvis@gmail.com>
2016-08-13 01:53:49 -0500

    oneplus2 : Remove commits breaking boot up
    Revert "oneplus2: Dalvik props from stock"
    
    This reverts commit b80f01df550508890a28faca172d26cecce8b95e.
    
    Revert "[TEST] OnePlus2: use OnePlus HWUI values"
    
    This reverts commit 25b844b82b9f76bd6a9a1832973af69a3d2f2745.
    
    Revert "oneplus2: use a bigger readahead size during bootup"
    
    This reverts commit 860377cfd0f3d6b0e04a9b3fd4d04141909e1e99.
    
    Revert "oneplus2: enable dexpreopt for userdebug"
    
    This reverts commit 3aef0a0ffb2dd8a1e7398a102597a8e6529463c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,11 +165,9 @@ USE_SENSOR_MULTI_HAL := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
-  endif
 endif
 
 # Wifi
